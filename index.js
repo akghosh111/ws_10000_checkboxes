4d94d2a0ccdc9f1a1ea2582ece7f596f7a3baac6
akghosh111 <ghoshkiran2017@gmail.com>
2026-04-30 15:57:22 +0530

    socket communication

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ async function main() {
 
     io.on("connection", (socket) => {
         console.log(`Socket connected`, { id: socket.id });
+
+        socket.on("client:checkbox:change", (data) => {
+            console.log(`[Socket:{socket.id}]:client:checkbox:change`, data)
+        })
     })
 
     
